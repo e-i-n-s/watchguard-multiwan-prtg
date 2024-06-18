229ae825b16ded81e9e8c83ea262c76920d385ae
Ph3nix <59368110+Ph3nix09@users.noreply.github.com>
2024-06-18 16:55:28 +0200

    Special characters in password
    Support for special characters in passwords (such as ?) that cause URL errors

--- watchguard-multiWAN.py
+++ watchguard-multiWAN.py
@@ -6,8 +6,9 @@ import xml.etree.ElementTree as ET
 import json
 import argparse
 from paesslerag_prtg_sensor_api.sensor.result import CustomSensorResult
+from urllib.parse import quote
 
-# Prepare data and web session
+# Préparation session web
 data = json.loads(sys.argv[1])
 parser = argparse.ArgumentParser()
 parser.add_argument("prtg", help="PRTG String")
@@ -19,26 +20,27 @@ cookiejar = http.cookiejar.CookieJar()
 cookiejar.clear_session_cookies()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookiejar),
                                      urllib.request.HTTPSHandler(context=context))
-# Firewall connection
+# Connexion au firewall WG
 parameters = json.loads(args.prtg)
 host = parameters.get('host')
 user = parameters.get('linuxloginusername')
 password = parameters.get('linuxloginpassword')
+encode_password = quote(password)
 baseURL = 'https://' + host + ':8080'
 
-url = baseURL + '/auth/login/' + user + '/' + password
-req = urllib.request.Request(url=url)
+url = baseURL + '/auth/login/' + user + '/' + encode_password
+req = urllib.request.Request(url)
 response = opener.open(req)
 
 url = baseURL + '/dashboard/dboard_get_interfaces?id=undefined'
 req = urllib.request.Request(url)
 response = opener.open(req)
 
-# JSON Response
+# Analyse de la réponse JSON
 interfaces = json.loads(response.read().decode('utf8'))
 xmlList = interfaces.get('interface_list')
 
-# XML
+# Analyse du XML
 list = ET.fromstring(xmlList)
 if list[0].tag == 'cluster':
     list = list.find('cluster').find('aggregate')
@@ -47,23 +49,23 @@ list_interfaces = list.find('network').find('interface_list')
 count_external_interfaces = 0
 failed_interfaces = []
 
-# Browse interfaces to check their status
+# Parcours des interfaces pour vérifier leur état
 for interface in list_interfaces:
     if interface.find('enabled').text == '1' and interface.find('zone').text == 'External':
         count_external_interfaces += 1
         if interface.find('wan_target_status').text == '0':
             failed_interfaces.append(interface.find('ifalias').text)
 
-# Concatenate interface names
-failed_interfaces_str = ', '.join(failed_interfaces) if failed_interfaces else 'None'
+# Concaténer les noms des interfaces en défaut
+failed_interfaces_str = ', '.join(failed_interfaces) if failed_interfaces else 'Aucune'
 
-# Adjusting messages and status
+# Ajustement des messages et du statut
 if len(failed_interfaces) == 0:
 	message = 'OK'
 else:
 	message = 'Failed WAN : ' + failed_interfaces_str
 
-# Sensor creation
+# Création du capteur
 csr = CustomSensorResult(text=message)
 csr.add_primary_channel(name="Status",
 						unit="WAN Failed",
@@ -71,5 +73,5 @@ csr.add_primary_channel(name="Status",
 						is_limit_mode=True,
 						limit_max_error=0.5)
 
-# Print result in JSON format for PRTG
+# Impression du résultat en format JSON pour PRTG
 print(csr.json_result)
